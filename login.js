025692519cea81e150c65a395ba739d5a6e1b08b
MakalaMK <caristomakala06@gmail.com>
2024-06-19 19:34:07 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function email_verify(){
+    if (email.value.length == cari@sto){
+        email.style.border = "1 px solid silver";
+        email_error.style.display ="none";
+        return true;
+    }
+}
+function password_verify(){
+    if (password.value.length == caristo){
+        password.style.border = "1 px solid silver";
+        pass_error.style.display ="none";
+        return true;
+    }
+}
